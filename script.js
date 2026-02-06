5a305f5b60cf62804c7b1995a63ddd6ca44f446f
ashoksree951-sketch <ashoksree951@gmail.com>
2026-02-06 14:58:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,50 +2,69 @@ const canvas = document.getElementById("wheel");
 const ctx = canvas.getContext("2d");
 
 const options = [
-  "Prize 1",
-  "Prize 2",
-  "Try Again",
-  "Bonus",
-  "Lucky!"
+  "Option 1",
+  "Option 2",
+  "Option 3",
+  "Option 4",
+  "Option 5",
+  "Option 6"
 ];
 
-const colors = ["#ff6384", "#36a2eb", "#ffce56", "#4caf50", "#9c27b0"];
-let angle = 0;
+const colors = [
+  "#e91e63",
+  "#3f51b5",
+  "#4caf50",
+  "#ff9800",
+  "#9c27b0",
+  "#00bcd4"
+];
+
+let startAngle = 0;
+const arc = (2 * Math.PI) / options.length;
 
 function drawWheel() {
-  const slice = 2 * Math.PI / options.length;
+  for (let i = 0; i < options.length; i++) {
+    const angle = startAngle + i * arc;
 
-  options.forEach((text, i) => {
     ctx.beginPath();
     ctx.fillStyle = colors[i];
     ctx.moveTo(200, 200);
-    ctx.arc(200, 200, 200, slice * i + angle, slice * (i + 1) + angle);
+    ctx.arc(200, 200, 200, angle, angle + arc);
     ctx.fill();
 
     ctx.save();
     ctx.translate(200, 200);
-    ctx.rotate(slice * i + slice / 2 + angle);
+    ctx.rotate(angle + arc / 2);
     ctx.fillStyle = "#fff";
     ctx.font = "16px Arial";
-    ctx.fillText(text, 80, 10);
+    ctx.fillText(options[i], 90, 10);
     ctx.restore();
-  });
+  }
 }
 
 function spin() {
-  const spinAngle = Math.random() * 2000 + 1000;
-  let current = 0;
-
-  const interval = setInterval(() => {
-    angle += 0.1;
-    current += 10;
+  const spinAngle = Math.random() * 3000 + 2000;
+  let spinTime = 0;
+  const spinInterval = setInterval(() => {
+    spinTime += 30;
+    startAngle += 0.3;
     ctx.clearRect(0, 0, 400, 400);
     drawWheel();
 
-    if (current >= spinAngle) {
-      clearInterval(interval);
+    if (spinTime >= spinAngle) {
+      clearInterval(spinInterval);
+      showResult();
     }
-  }, 20);
+  }, 30);
+}
+
+function showResult() {
+  const degrees = (startAngle * 180) / Math.PI + 90;
+  const index = Math.floor(
+    (360 - (degrees % 360)) / (360 / options.length)
+  );
+  document.getElementById("result").innerText =
+    "Result: " + options[index];
 }
 
 drawWheel();
